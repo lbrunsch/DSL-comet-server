c42b2663f473191e6f6d6a27892faab1f278446f
lbrunsch <leabrunschwig@gmail.com>
2020-06-10 14:35:56 +0200

    added some code from the original code + mongoose methods

--- app.js
+++ app.js
@@ -1080,28 +1080,50 @@ router.post("/login", function(req,res){
   });
 });
 
+//========================================================
+//=================    Default route   ===================
+//========================================================
+
+//Default route
+router.get("*", function(req, res){
+	//res.render("error", {code:404, message:"Upps. Page not found"});
+	endResponse(res);
+});
+
+
+
+app.use(router);
 
 //========================================================
 //================    MONGOOSE    ========================
 //========================================================
 
 
+var port = process.env.PORT || 8080;
+console.log("Port: "+ port);
+
+
 //Connection events
-//mongoose.connection.once("open", function(){
-//	console.log("We're connected! Start listening...");
-//});
+mongoose.connection.once("open", function(){
+	console.log("We're connected! Start listening...");
 
-//mongoose.connection.on("error", function(err){
-//	console.log("Error");
-//	mongoose.connection.close();
-//	process.exit(1);
-//});
+	//Start listening
+	app.listen(port, function() {
+		console.log("Node server running, listening on port " +port);
+	});
+});
+
+mongoose.connection.on("error", function(err){
+	console.log("Error");
+	mongoose.connection.close();
+	process.exit(1);
+});
 
 
-//mongoose.connection.on("disconnected", function(){
-//	console.log("Mongoose disconnected");
-//	mongoose.connection.close();
-//});
+mongoose.connection.on("disconnected", function(){
+	console.log("Mongoose disconnected");
+	mongoose.connection.close();
+});
 
 var str = "mongodb://" +username+":"+password + "@ds115546.mlab.com:15546/diagrameditor";
 
