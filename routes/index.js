d275340846c851976f924806eb02522410eb2ab5
lbrunsch <leabrunschwig@gmail.com>
2020-06-10 14:09:52 +0200

    palettes, models, ecores

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DSL-Comet' });
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'DSL-Comet' });
+//});
+
+router.get('/', function(req, res) {
+	console.log("/");
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	console.log("ip: " + req.ip)
+	res.render('index', { title: 'DSL-Comet' });
+	//endResponse(res);
 });
 
 module.exports = router;
