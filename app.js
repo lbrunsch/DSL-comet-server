710139c207ff4e377f8de4a4493e27d0d27e896f
lbrunsch <leabrunschwig@gmail.com>
2020-06-12 18:32:57 +0200

    favicon change server-favicon -> serve-favicon

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const uriUtil = require('mongodb-uri');
 var methodOverride = require("method-override");
-var favicon = require('server-favicon');
+var favicon = require('serve-favicon');
 
 //mongoose entity vars
 var Palette = require("./models/palette");
