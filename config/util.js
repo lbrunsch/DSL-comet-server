342eb562f3818628d551a0205493bc4b73c00479
lbrunsch <leabrunschwig@gmail.com>
2020-06-14 22:57:15 +0200

    remove setHeaders for Json responses

--- config/util.js
+++ config/util.js
@@ -5,12 +5,12 @@
 module.exports = {
   sendJsonResponse: function (res, element){
   	//console.log("en sendJsonResponse. Element = " + element);
-  	res.setHeader("Content-Type", "application/json");
+  	//res.setHeader("Content-Type", "application/json");
   	res.send(JSON.stringify({code: "200", array:element}));
   },
   sendJsonError: function (res, text){
   	//console.log("En sendJsonError. Text = "+ text);
-  	res.setHeader("Content-Type", "application/json");
+  	//res.setHeader("Content-Type", "application/json");
     console.log(text);
   	res.send(JSON.stringify({error: text}));
   },
