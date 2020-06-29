c19d76551337a83545739d66d62b6f5e3058c6f3
lbrunsch <leabrunschwig@gmail.com>
2020-06-29 23:50:55 +0200

    correction mixins' link for DSL-comet and correctio in routes.js req.session instead of req.sessions

--- config/routes.js
+++ config/routes.js
@@ -39,7 +39,7 @@ module.exports = function(app){
     const username = req.session.username;
     res.locals.isAuthenticated = req.session.isLoggedIn;
     res.locals.username = username;
-    res.locals.roleOnWeb = req.sessions.roleOnWeb;
+    res.locals.roleOnWeb = req.session.roleOnWeb;
     //res.locals.csrfToken = req.csrfToken();
     next();
   });
